4b4b5070c71aa6488492f01a239ea78ccaa6d826
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-06 23:12:27 +0530

    fetching images from firebase storage

--- lib/Screens/HomeScreen 1st/carousel_slider.dart
+++ lib/Screens/HomeScreen 1st/carousel_slider.dart
@@ -2,12 +2,9 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-
 class FirebaseApi {
   static Future<List<String>> _getDownloadLinks(List<Reference> refs) =>
-
       Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
-
 
   static Future<List<FirebaseFile>> listAll(String path) async {
     final ref = FirebaseStorage.instance.ref(path);
@@ -18,12 +15,12 @@ class FirebaseApi {
     return urls
         .asMap()
         .map((index, url) {
-      final ref = result.items[index];
-      final file = FirebaseFile(ref: ref, url: url);
+          final ref = result.items[index];
+          final file = FirebaseFile(ref: ref, url: url);
 
-      return MapEntry(index, file);
-
-    }).values
+          return MapEntry(index, file);
+        })
+        .values
         .toList();
   }
 }
@@ -32,7 +29,7 @@ class FirebaseFile {
   final Reference ref;
   final String url;
 
-  const FirebaseFile({ required this.ref, required this.url});
+  const FirebaseFile({required this.ref, required this.url});
 }
 
 final List<String> imgList = [
@@ -50,6 +47,8 @@ class Carousel extends StatefulWidget {
 
 class _CarouselState extends State<Carousel> {
   int _current = 0;
+
+
   // static Future<List<String>> _getDownloadLinks(List<Reference> refs) => Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
   // static Future<List<FirebaseFile>> listAll() async {
   //   final ref = FirebaseStorage.instance.ref('carousel/');
@@ -68,14 +67,16 @@ class _CarouselState extends State<Carousel> {
   //   }).values
   //       .toList();
   // }
-  void getImage() async{
-    final  ref = await FirebaseStorage.instance.ref('carousel/').listAll();
+  void getImage() async {
+    final ref = await FirebaseStorage.instance.ref('carousel/').listAll();
     print(ref);
   }
+
   final CarouselController _controller = CarouselController();
 
   //
   late Future<List<FirebaseFile>> futureFiles;
+  final controller = PageController();
 
   @override
   void initState() {
@@ -114,72 +115,13 @@ class _CarouselState extends State<Carousel> {
                 aspectRatio: 16 / 9,
                 autoPlay: true,
                 enlargeCenterPage: true,
-                autoPlayAnimationDuration:
-                const Duration(milliseconds: 250),
+                autoPlayAnimationDuration: const Duration(milliseconds: 250),
                 onPageChanged: (index, reason) {
                   setState(() {
                     _current = index;
                   });
                 }),
           ),
-        ),
-    //Image.network('https://firebasestorage.googleapis.com/v0/b/cultevents-de04a.appspot.com/o/carousel%2F14358.jpg?alt=media&token=17a3d95b-cf34-4102-859e-6f6ac9138942'),
-
-    FutureBuilder<List<FirebaseFile>>(
-          future: futureFiles,
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              default:
-                if (snapshot.hasError) {
-                  return const Center(
-                    child: Text("Some error occured"),
-                  );
-                } else {
-                  final files = snapshot.data!;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: files.length,
-                        itemBuilder: (context, index) {
-                          final file = files[index];
-                          return buildFile(context, file);
-                        },
-                      )
-                    ],
-                  );
-                }
-            }
-          },
-        ),
-        Row(
-          children: [
-            SizedBox(
-              height: 150,
-              width: MediaQuery.of(context).size.width,
-              child: CarouselSlider(
-                items: imageSliders,
-                carouselController: _controller,
-                options: CarouselOptions(
-                    viewportFraction: 1,
-                    aspectRatio: 16 / 9,
-                    autoPlay: true,
-                    enlargeCenterPage: true,
-                    autoPlayAnimationDuration:
-                        const Duration(milliseconds: 250),
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _current = index;
-                      });
-                    }),
-              ),
-            ),
-          ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -201,16 +143,62 @@ class _CarouselState extends State<Carousel> {
             );
           }).toList(),
         ),
+        //Image.network('https://firebasestorage.googleapis.com/v0/b/cultevents-de04a.appspot.com/o/carousel%2F14358.jpg?alt=media&token=17a3d95b-cf34-4102-859e-6f6ac9138942'),
+
+        FutureBuilder<List<FirebaseFile>>(
+          future: futureFiles,
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text("Some error occurred"),
+                  );
+                } else {
+                  final files = snapshot.data!;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 200,
+                        child: PageView.builder(
+                          controller: controller,
+                          scrollDirection: Axis.horizontal,
+                          //shrinkWrap: true,
+                         itemCount: files.length,
+                          itemBuilder: (context, index) {
+                            final file = files[index % files.length];
+                            return buildFile(context, file);
+                          },
+                        ),
+                      )
+                    ],
+                  );
+                }
+            }
+          },
+        ),
 
       ],
     );
   }
 
-  Widget buildFile(BuildContext context, FirebaseFile file) => Image.network(
-        file.url,
-        width: 100,
-        height: 100,
-        fit: BoxFit.cover,
+  Widget buildFile(BuildContext context, FirebaseFile file) => AnimatedContainer(
+    duration: const Duration(milliseconds: 350),
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Image.network(
+            file.url,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
       );
 }
-
