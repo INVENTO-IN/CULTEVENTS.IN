dc58be0e67bd17f410d97404fa5b82bb450b4695
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-07 12:08:34 +0530

    carousel

--- lib/Screens/HomeScreen 1st/carousel_slider.dart
+++ lib/Screens/HomeScreen 1st/carousel_slider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -47,7 +49,12 @@ class Carousel extends StatefulWidget {
 
 class _CarouselState extends State<Carousel> {
   int _current = 0;
-
+  int _currentPage = 0;
+  late Timer _timer;
+  final bool isActive = false;
+  final PageController _pageController = PageController(
+    initialPage: 0,
+  );
 
   // static Future<List<String>> _getDownloadLinks(List<Reference> refs) => Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
   // static Future<List<FirebaseFile>> listAll() async {
@@ -67,26 +74,63 @@ class _CarouselState extends State<Carousel> {
   //   }).values
   //       .toList();
   // }
-  void getImage() async {
-    final ref = await FirebaseStorage.instance.ref('carousel/').listAll();
-    print(ref);
-  }
+  // void getImage() async {
+  //   final ref = await FirebaseStorage.instance.ref('carousel/').listAll();
+  //   print(ref);
+  // }
 
   final CarouselController _controller = CarouselController();
 
   //
   late Future<List<FirebaseFile>> futureFiles;
-  final controller = PageController();
+
+
+  Future<List<String>> getirebaseImage(String path) async {
+    final ref = FirebaseStorage.instance.ref(path);
+    final result = await ref.listAll();
+    Future<List<String>> _getDownloadLinks(List<Reference> refs) =>
+        Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
+    final urls = await _getDownloadLinks(result.items);
+    //print(urls);
+    return urls;
+  }
+
+
+
+  late Future ImageList;
+  late Future ImgList = ImageList;
 
   @override
   void initState() {
     super.initState();
     futureFiles = FirebaseApi.listAll('carousel/');
+     ImageList = getirebaseImage('carousel/');
+    _timer = Timer.periodic(const Duration(seconds: 4), (Timer timer) {
+      if (_currentPage < 2) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+        
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration:const  Duration(milliseconds: 500),
+        curve: Curves.easeIn,
+      );
+    });
   }
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+  //final List<String> imgList = ImageList ;
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> imageSliders = imgList
+    //final List<Widget> Image = ImageList
+    final List<Widget> imageSliders =  imgList
         .map((image) => Padding(
               padding: const EdgeInsets.all(8.0),
               child: ClipRRect(
@@ -104,45 +148,26 @@ class _CarouselState extends State<Carousel> {
 
     return Column(
       children: [
-        SizedBox(
-          height: 150,
-          width: MediaQuery.of(context).size.width,
-          child: CarouselSlider(
-            items: imageSliders,
-            carouselController: _controller,
-            options: CarouselOptions(
-                viewportFraction: 1,
-                aspectRatio: 16 / 9,
-                autoPlay: true,
-                enlargeCenterPage: true,
-                autoPlayAnimationDuration: const Duration(milliseconds: 250),
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _current = index;
-                  });
-                }),
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: imgList.asMap().entries.map((entry) {
-            return GestureDetector(
-              onTap: () => _controller.animateToPage(entry.key),
-              child: Container(
-                width: 8.0,
-                height: 8.0,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: (Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black)
-                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-              ),
-            );
-          }).toList(),
-        ),
+        // SizedBox(
+        //   height: 150,
+        //   width: MediaQuery.of(context).size.width,
+        //   child: CarouselSlider(
+        //     items: imageSliders,
+        //     carouselController: _controller,
+        //     options: CarouselOptions(
+        //         viewportFraction: 1,
+        //         aspectRatio: 16 / 9,
+        //         autoPlay: true,
+        //         enlargeCenterPage: true,
+        //         autoPlayAnimationDuration: const Duration(milliseconds: 250),
+        //         onPageChanged: (index, reason) {
+        //           setState(() {
+        //             _current = index;
+        //           });
+        //         }),
+        //   ),
+        // ),
+
         //Image.network('https://firebasestorage.googleapis.com/v0/b/cultevents-de04a.appspot.com/o/carousel%2F14358.jpg?alt=media&token=17a3d95b-cf34-4102-859e-6f6ac9138942'),
 
         FutureBuilder<List<FirebaseFile>>(
@@ -166,10 +191,10 @@ class _CarouselState extends State<Carousel> {
                       SizedBox(
                         height: 200,
                         child: PageView.builder(
-                          controller: controller,
+                          controller: _pageController,
                           scrollDirection: Axis.horizontal,
                           //shrinkWrap: true,
-                         itemCount: files.length,
+                          //itemCount: files.length,
                           itemBuilder: (context, index) {
                             final file = files[index % files.length];
                             return buildFile(context, file);
@@ -183,14 +208,83 @@ class _CarouselState extends State<Carousel> {
           },
         ),
 
+       const  SizedBox(
+          height: 10,
+
+        ),
+
+        // FutureBuilder<List<FirebaseFile>>(
+        //   future: futureFiles,
+        //   builder: (context, snapshot) {
+        //     switch (snapshot.connectionState) {
+        //       case ConnectionState.waiting:
+        //         return const Center(
+        //           child: CircularProgressIndicator(),
+        //         );
+        //       default:
+        //         if (snapshot.hasError) {
+        //           return const Center(
+        //             child: Text("Some error occurred"),
+        //           );
+        //         } else {
+        //           final files = snapshot.data!;
+        //           return Column(
+        //             crossAxisAlignment: CrossAxisAlignment.start,
+        //             children: [
+        //               SizedBox(
+        //                 height: 10,
+        //                 //width: 50,
+        //                 child: PageView.builder(
+        //                   //controller: _pageController,
+        //                   scrollDirection: Axis.horizontal,
+        //                   //shrinkWrap: true,
+        //                   //itemCount: files.length,
+        //                   itemBuilder: (context, index) {
+        //                     final file = files[index % files.length];
+        //                     return Row(
+        //                       children:  [
+        //                         ...List.generate(files.length, (index) => Indicator(isActive))
+        //
+        //
+        //                       ],
+        //                     );
+        //                   },
+        //                 ),
+        //               )
+        //             ],
+        //           );
+        //         }
+        //     }
+        //   },
+        // ),
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: imgList.asMap().entries.map((entry) {
+        //     return GestureDetector(
+        //       onTap: () => _controller.animateToPage(entry.key),
+        //       child: Container(
+        //         width: 8.0,
+        //         height: 8.0,
+        //         margin:
+        //         const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+        //         decoration: BoxDecoration(
+        //             shape: BoxShape.circle,
+        //             color: (Theme.of(context).brightness == Brightness.dark
+        //                 ? Colors.white
+        //                 : Colors.black)
+        //                 .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+        //       ),
+        //     );
+        //   }).toList(),
+        // ),
       ],
     );
   }
 
-  Widget buildFile(BuildContext context, FirebaseFile file) => AnimatedContainer(
-    duration: const Duration(milliseconds: 350),
+  Widget buildFile(BuildContext context, FirebaseFile file) =>
+      AnimatedContainer(
+        duration: const Duration(milliseconds: 350),
         margin: const EdgeInsets.symmetric(horizontal: 10),
-        
         child: ClipRRect(
           borderRadius: BorderRadius.circular(15),
           child: Image.network(
@@ -201,4 +295,34 @@ class _CarouselState extends State<Carousel> {
           ),
         ),
       );
+  // Widget indicator(BuildContext context, FirebaseFile file) =>
+  //     AnimatedContainer(
+  //       duration: const Duration(milliseconds: 350),
+  //       margin: const EdgeInsets.symmetric(horizontal: 10),
+  //       child: Container(
+  //         width: isActive ? 22.0 : 8.0,
+  //         height: 8.0,
+  //         decoration: BoxDecoration(
+  //           color: isActive ? Colors.black : Colors.grey,
+  //           borderRadius: BorderRadius.circular(10)
+  //         ),
+  //       )
+  //     );
 }
+// class Indicator extends StatelessWidget {
+//   final bool isActive;
+//    const Indicator(  this.isActive);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: isActive ? 22.0 : 8.0,
+//       height: 8.0,
+//       decoration: BoxDecoration(
+//           color: isActive ? Colors.black : Colors.grey,
+//           borderRadius: BorderRadius.circular(10)
+//       ),
+//     );
+//   }
+// }
+
