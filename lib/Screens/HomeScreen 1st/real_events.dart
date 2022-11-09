1e0de987f58a535e5ea2c0e04f0b33a7dbc09c84
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-09 12:35:45 +0530

    listview in homescreen

--- lib/Screens/HomeScreen 1st/real_events.dart
+++ lib/Screens/HomeScreen 1st/real_events.dart
@@ -21,26 +21,63 @@ class RealEvents extends StatelessWidget {
         final data = snapshot.data!.docs;
         return Container(
           padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 5),
-          height: 350,
+          height: 380,
           width: MediaQuery.of(context).size.width,
           child: ListView.builder(
+            physics: const BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
             itemCount: data.length,
             itemBuilder: (context, index) {
+              final image = data[index]['image'];
+              final text = data[index]['title'];
               return Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10, top: 0),
-                child:  Container(
-                  height: 280,
-                  width: 230,
-                  child: Stack(
-
-                    children: [
-                      Image.network(
-                        data[index]['image'],
-                        fit: BoxFit.cover,
-                      )
-                    ],
-                  ),
+                padding: const EdgeInsets.only(
+                    left: 15, right: 10, bottom: 10, top: 0),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(image),
+                        ),
+                      ),
+                      height: 380,
+                      width: 280,
+                      child: Container(
+                        //height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            gradient: LinearGradient(
+                                begin: FractionalOffset.center,
+                                end: FractionalOffset.bottomCenter,
+                                colors: [
+                                  Colors.transparent.withOpacity(0.0),
+                                  Colors.black
+                                ],
+                                stops: const [
+                                  0.5,
+                                  1.0
+                                ])),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            text,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               );
             },
