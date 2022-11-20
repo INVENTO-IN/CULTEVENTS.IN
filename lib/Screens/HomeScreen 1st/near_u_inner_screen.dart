051c37a70cf906f5f871ed069bb4a73ff9c66f58
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-20 12:58:39 +0530

    inner page of near you

--- lib/Screens/HomeScreen 1st/near_u_inner_screen.dart
+++ lib/Screens/HomeScreen 1st/near_u_inner_screen.dart
@@ -19,7 +19,7 @@ class NearYouInner extends StatelessWidget {
     TextStyle textStyle = const TextStyle(
         color: Colors.black,
         fontWeight: FontWeight.w500,
-        fontSize: 18,
+        fontSize: 14,
         fontFamily: 'Poppins');
     //print(uid);
     return Scaffold(
@@ -40,7 +40,7 @@ class NearYouInner extends StatelessWidget {
           style: const TextStyle(
               color: Colors.black,
               fontFamily: 'Poppins',
-              fontSize: 18,
+              fontSize: 16,
               fontWeight: FontWeight.w600),
         ),
       ),
@@ -60,35 +60,37 @@ class NearYouInner extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
             width: width,
             height: MediaQuery.of(context).size.height,
-            child: GridView.builder(
+            child: ListView.builder(
+
               physics: const BouncingScrollPhysics(),
               shrinkWrap: true,
               scrollDirection: Axis.vertical,
               itemCount: data.length,
               itemBuilder: (ctx, index) {
                 return Padding(
-                  padding: const EdgeInsets.all(15.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Column(
-                    //mainAxisAlignment: MainAxisAlignment.start,
+
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(
-                        height: 180,
-                        width: width,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                            data[index]['image'],
-                            errorBuilder: (ctx, object, stackTrace) {
-                              return const Text("error");
-                            },
-                            fit: BoxFit.cover,
+                      FittedBox(
+                        fit: BoxFit.cover,
+                        child: SizedBox(
+                          height: 400,
+                          width: width,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(
+                              data[index]['image'],
+                              errorBuilder: (ctx, object, stackTrace) {
+                                return const Text("error");
+                              },
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
-                      const SizedBox(
-                        height: 8,
-                      ),
+
                       Container(
                         alignment: Alignment.centerLeft,
                         child: Text(
@@ -97,25 +99,21 @@ class NearYouInner extends StatelessWidget {
                           style: textStyle,
                         ),
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
+
                       Container(
                         alignment: Alignment.centerLeft,
                         child: Text(
                           data[index]['location'],
                           textAlign: TextAlign.start,
                           style: const TextStyle(
-                              fontSize: 12,
+                              fontSize: 11,
                               fontFamily: 'Poppins',
                               color: Colors.black87,
                               fontWeight: FontWeight.w400),
                           //style: Theme.of(context).textTheme.subtitle2,
                         ),
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
+
                       Container(
                         alignment: Alignment.centerLeft,
                         child: Text(
@@ -126,14 +124,13 @@ class NearYouInner extends StatelessWidget {
                           //style:
                         ),
                       ),
-                      const Divider(),
+
                     ],
                   ),
                 );
-              }, gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
+              },
             ),
-            ),
+
           );
         },
       ),
